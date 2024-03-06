0c7f00af83b56cad53c13bdffcb7778c86dfc168
Anushree Bhuskute <102479736+Anushree-B@users.noreply.github.com>
2024-03-06 22:54:14 +0530

    Create Q2_solution.py

---
+++ Data Science/Q2_solution.py
@@ -0,0 +1,17 @@
+def check_year(year):
+  flag = 0
+  if year%4 == 0:
+    flag = 1
+    if year%100 == 0:
+      flag = 0
+      if year%400 == 0:
+        flag = 1
+  if flag == 1:
+    return 1
+  return 0
+
+year = int(input("Enter year: "))
+if(check_year(year)):
+  print("Leap year")
+else:
+  print("Not a leap year")
